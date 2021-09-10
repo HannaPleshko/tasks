9a2df160e5d8853a0e66b1f559097cc0d39c5dab
IBA\PleshkoH <anna-pleshko2000@mail.ru>
2021-09-10 14:21:29 +0300

    Added tasks/src/tasks/tasks.service.js; getAllTasks(), getTask(), updateTask(), deleteTask()

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
-const express = require('express')
-const app = require('./src/app');
+const app = require('./src/app')
 
 const port = process.env.PORT
 app.listen(port, () => console.log(`Server is running on port ${port}...`))
