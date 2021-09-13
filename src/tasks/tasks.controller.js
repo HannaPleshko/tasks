c9090eea9c10df72f2e9be42a3b6b5601e11c14f
IBA\PleshkoH <anna-pleshko2000@mail.ru>
2021-09-13 09:49:36 +0300

    review controller. added res.json(); destructuring assignment

--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -4,30 +4,29 @@ const service = require('./tasks.service')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.status(200).send(service.getAllTasks())
+    res.status(200).json(service.getAllTasks())
 })
 router.post('/', (req, res) => {
     console.log(req.body)
     res.send(`Success ${res.statusCode} ${req.originalUrl}`)
 })
 router.get('/:id', (req, res) => {
-    const requiredData = service.getTask(req.params.id)
-    if (requiredData) res.status(200).send(requiredData)
+    const {id} = req.params
+    const requiredData = service.getTask(id)
+    if (requiredData) res.status(200).json(requiredData)
     else res.status(404).send('The task with the required identifier is missing')
 })
 router.put('/:id', (req, res) => {
-    const taskData = {
-        id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-    }
-    const requiredData = service.updateTask(taskData.id, taskData.title, taskData.description)
-    if (requiredData) res.status(200).send(requiredData)
+    const {id} = req.params
+    const {title, description} = req.body
+    const requiredData = service.updateTask(id, title, description)
+
+    if (requiredData) res.status(200).json(requiredData)
     else res.status(404).send('The task with the required identifier is missing')
 })
 router.delete('/:id', (req, res) => {
     const requiredData = service.deleteTask(req.params.id)
-    if (requiredData) res.status(200).send(requiredData)
+    if (requiredData) res.status(200).json(requiredData)
     else res.status(404).send('The task with the required identifier is missing')
 })
 
