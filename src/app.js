dbdd9d15faea1bfd66519874aad41454e6b3a69c
IBA\PleshkoH <anna-pleshko2000@mail.ru>
2021-09-13 14:32:27 +0300

    added validation to create a task

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
-const {handleError, ErrorHandler } = require('./helpers/error') 
+const {handleError, ErrorHandler} = require('./helpers/error') 
 const express = require('express')
 const bodyParser = require('body-parser')
 const tasks = require('./tasks/tasks.controller')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
 app.get('/', (req, res) => {
     res.send(`Success ${res.statusCode} ${req.originalUrl}`)
 })
 app.get('/error', (req, res) => {
-    throw new ErrorHandler(500, 'Internal server error');
+    throw new ErrorHandler(500, 'Internal server error')
 })
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use('/tasks', tasks)
 app.use((err, req, res, next) => handleError(err, res))
 
