811e02863c499b3d10a09072908f8d75f837802d
IBA\PleshkoH <anna-pleshko2000@mail.ru>
2021-10-20 17:29:45 +0300

    feature/#21: auth; Remove dist directory

--- dist/src/helpers/response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.buildResponse = void 0;
-var buildResponse = function (res, st, mess) {
-    res.status(st);
-    res.json(mess);
-};
-exports.buildResponse = buildResponse;
-//# sourceMappingURL=response.js.map
